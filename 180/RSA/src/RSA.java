cdc9c0c077258dcb4a4bc0c19a4e07f1066a1a17
Josh Jacobsen <josajacobsen@gmail.com>
2016-03-30 09:49:15 -0500

    Submitting the RSA Encryption Homework

--- 180/RSA/src/RSA.java
+++ 180/RSA/src/RSA.java
@@ -13,29 +13,36 @@ public class RSA {
         BigInteger d;
 
         Random rand = new Random();
-        p = new BigInteger(2048, 1, rand);
-        q = new BigInteger(2048, 1, rand);
+        p = BigInteger.probablePrime(2048,rand);
+        q = BigInteger.probablePrime(2048,rand);
+        //p = new BigInteger(200, 1, rand);
+        //q = new BigInteger(200, 1, rand);
 
         n = p.multiply(q);
-
         p = p.subtract((new BigInteger(Integer.toString(1))));
         q = q.subtract((new BigInteger(Integer.toString(1))));
         phi = p.multiply(q);
 
-//        e = 3;
-//        while(phi%e == 0){
-//            e +=2;
-//        }
-
-        e = new BigInteger((Integer.toString(3)));
-        while(phi.mod(e).equals("0")){
-            e.add((new BigInteger(Integer.toString(2))));
+        e = new BigInteger("3");
+        while(phi.mod(e).equals(BigInteger.ZERO)){
+            e = e.add((new BigInteger("2")));
         }
-        System.out.println("P: " + p);
-        System.out.println("Q: " + q);
-        System.out.println("N: " + n);
-        System.out.println("Phi: " + phi);
-        System.out.println("E: " + e);
-        //System.out.println("D: " + d);
+
+        d = e.modInverse(phi);
+
+        System.out.println("p: " + p);
+        System.out.println("q: " + q);
+        System.out.println("n: " + n);
+        System.out.println("phi: " + phi);
+        System.out.println("e: " + e);
+        System.out.println("Public Key: " + e + ", " + n);
+        System.out.println("Sum of the bits: " + p.bitCount() + q.bitCount());
+
+        BigInteger m = new BigInteger("123456789");
+        BigInteger r = m.modPow(e, n);
+        BigInteger m2 = r.modPow(d, n);
+        System.out.println("m: " + m);
+        System.out.println("r: " + r);
+        System.out.println("m2: " + m2);
     }
 }
