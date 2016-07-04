2f423602f76b39a72436f853f059cda7b112a547
Josh Jacobsen <josajacobsen@gmail.com>
2016-07-04 14:40:28 -0500

    Adding the starting of Sonny the reddit bot

---
+++ Sonny_RB/Sonny.py
@@ -0,0 +1,7 @@
+app_id = "a2vJ--w9paRKrw"
+app_secret = "OD2R_DC9xc_MVAY-moEQSKDQ_aM"
+app_uri = "https://127.0.0.1:65010/authorize_callback"
+app_ua = "Comment automatic replier v1.0.0 (Created by _Sonny)"
+app_scopes = 'account creddits edit flair history identity livemanage modconfig modcontributors modflair modlog modothers modposts modself modwiki mysubreddits privatemessages read report save submit subscribe vote wikiedit wikiread'
+app_account_code = "eQBTKRkGO_A_vWzefAmfAuP6WsU"
+app_refresh = "zh0nhOch7BkmxAIYeLMmqLYm7F8"
